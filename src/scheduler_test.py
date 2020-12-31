0cf3c2df1a9fea6fdc63c57b291fc8a680683a1f
Ankur Gupta <rukna1000@gmail.com>
2020-12-31 14:52:15 -0500

    Test on call functionality

--- src/scheduler_test.py
+++ src/scheduler_test.py
@@ -1,8 +1,17 @@
+from parameterized import parameterized
+
+import scheduler
 import unittest
 
 
 class SchedulerTestCase(unittest.TestCase):
-  pass
+  @parameterized.expand([
+    ('single list', ['test_user'], 'test_user'),
+    ('multiple users', ['first', 'second', 'third'], 'first')
+  ])
+  def test_on_call(self, name, users, on_call_user):
+    sch = scheduler.Scheduler(users)
+    self.assertEqual(sch.on_call, on_call_user)
 
 
 if __name__ == '__main__':
